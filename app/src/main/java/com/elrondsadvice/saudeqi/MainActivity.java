192b391d7271a73762a9236fe30ea5797b85666b
Walter Junior <juniordeonisio@gmail.com>
2017-03-14 19:22:56 -0300

    Inital commit

---
+++ app/src/main/java/com/elrondsadvice/saudeqi/MainActivity.java
@@ -0,0 +1,20 @@
+package com.elrondsadvice.saudeqi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference firebaseReference = FirebaseDatabase.getInstance().getReference();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firebaseReference.child("teste").setValue(100);
+    }
+}
