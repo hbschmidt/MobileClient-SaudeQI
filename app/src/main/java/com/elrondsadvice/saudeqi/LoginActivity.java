46375ded7e8219928f872bc74d01e252a924d968
henriquebelz <henriquebelz@gmail.com>
2017-04-20 21:25:16 -0300

    interfaces novas

--- app/src/main/java/com/elrondsadvice/saudeqi/LoginActivity.java
+++ app/src/main/java/com/elrondsadvice/saudeqi/LoginActivity.java
@@ -1,13 +1,42 @@
 package com.elrondsadvice.saudeqi;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText edtLogin;
+    private EditText edtSenha;
+    private Button btnLogar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        edtLogin = (EditText) findViewById(R.id.edtLogin);
+        edtSenha = (EditText) findViewById(R.id.edtSenha);
+        btnLogar = (Button) findViewById(R.id.btnLogar);
+    }
+
+    private  boolean validarCamposLogin(String login, String senha)
+    {
+        boolean resultado = true;
+        if(login == null || "".equals(login))
+        {
+            edtLogin.setError("Campo Login Obrigatório");
+            resultado =  false;
+        }
+
+        if(senha == null || "".equals(senha))
+        {
+            edtSenha.setError("Campo Senha Obrigatório");
+            resultado = false;
+        }
+
+        return resultado;
     }
 }
